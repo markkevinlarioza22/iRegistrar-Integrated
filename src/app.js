9df2647df9f4a6015eafdca6400d7d3b8291bd06
markkevinlarioza22 <markkevinlarioza886@gmail.com>
2025-12-03 10:16:22 +0800

    Clean app.js - fix duplicate analyticsRoutes

--- src/app.js
+++ src/app.js
@@ -3,21 +3,23 @@ const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+
 dotenv.config();
 
+const app = express();
+
+// Route imports
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const requestRoutes = require('./routes/requestRoutes');
 const analyticsRoutes = require('./routes/analyticsRoutes');
-
-const app = express();
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve frontend
+// Serve frontend static files
 app.use(express.static(path.join(__dirname, '../public')));
 
 // API routes
@@ -26,9 +28,9 @@ app.use('/api/users', userRoutes);
 app.use('/api/requests', requestRoutes);
 app.use('/api/analytics', analyticsRoutes);
 
-// Catch-all for SPA or static pages
-const analyticsRoutes = require('./routes/analyticsRoutes');
-app.use('/api/analytics', analyticsRoutes);
-
+// Catch-all route for SPA
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 module.exports = app;
